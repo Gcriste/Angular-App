fe6d970caa1b6f29c16a385e0055fe20010e10dc
Griffin <griffincriste@gmail.com>
2020-04-20 13:30:43 -0500

    properties and mthods

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -13,19 +13,42 @@ import { Component } from '@angular/core';
 
 export class UserComponent {
     //properties
-    firstName = 'John';
-    lastName = 'Doe';
-    age = 30;
-    address = {
-        street: '50 Main Street',
-        city: 'Nashville',
-        state: 'TN'
-    };
+    firstName: string;
+    lastName: string;
+    age: number;
+    address;
+
+    foo: any;
+    hasKids: boolean;
+    numberArray: number[];
+    stringArray: string[];
+    mixedArray: any[];
+    myTuple: [string, number, boolean];
+    unusuable: void;
+    u: undefined;
+    n: null;
 
 
     //methods
     constructor() {
-      
+      this.firstName = 'John';
+      this.lastName = 'Doe';
+      this.age = 30;
+      this.address = {
+          street: '50 Main Street',
+          city: 'Nashville',
+          state: 'TN'
+      }
+
+      this.foo = true;
+      this.hasKids = true;
+      this.numberArray = [1,4,5];
+      this.stringArray = ['Griffin', 'Joe', 'Marvin'];
+      this.mixedArray = ['Griffin', undefined, 4, true];
+      this.myTuple = ['John', 4, true ];
+      this.unusuable = undefined;
+      this.u = undefined;
+      this.n = null;
     }
 
     showAge(){
