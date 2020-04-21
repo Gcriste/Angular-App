5f9562f261415c42d71c202004143808f3389d50
Griffin <griffincriste@gmail.com>
2020-04-21 14:46:06 -0500

    ngIf and ngFor

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -8,51 +8,53 @@ import { User } from '../../models/User';
 })
 export class UsersComponent implements OnInit {
   users: User[];
+  showExtended: boolean = true;
+  loaded: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
-    this.users = [
-       {
-        firstName: 'Griffin',
-        lastName: 'Criste',
-        age: 29,
-        address:{
-            street: '50 main street',
-            city: 'Nashville',
-            state: 'TN'
-        }
-    },
-     {
-      firstName: 'Kevin',
-      lastName: 'Bacion',
-      age: 35,
-      address:{
-          street: '225 school street',
-          city: 'Boston',
-          state: 'MA'
-      }
-  },
-   {
-    firstName: 'Karen',
-    lastName: 'Williams',
-    age: 23,
-    address:{
-        street: '583 Mill Street',
-        city: 'Austin',
-        state: 'TX'
-    }
+
+    // setTimeout(() => {
+      this.users = [
+      {
+       firstName: 'Griffin',
+       lastName: 'Criste',
+       age: 29,
+       address:{
+           street: '50 main street',
+           city: 'Nashville',
+           state: 'TN'
+       }
+   },
+    {
+     firstName: 'Kevin',
+     lastName: 'Bacion',
+     age: 35,
+     address:{
+         street: '225 school street',
+         city: 'Boston',
+         state: 'MA'
+     }
+ },
+  {
+   firstName: 'Karen',
+   lastName: 'Williams',
+   age: 23,
+   address:{
+       street: '583 Mill Street',
+       city: 'Austin',
+       state: 'TX'
+   }
 }
-    ];
+   ];
+   this.loaded = true;
+
+    // }, 2000)
+    
     this.addUser({
       firstName: 'David',
-      lastName: 'Jackson',
-      age: 49,
-      address:{
-          street: '608 Wallabe Way',
-          city: 'Baltimore',
-          state: 'MD'
-      }
+      lastName: 'Jackson'
   })
   }
 
