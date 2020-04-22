7b8a10ab9deb271278f4ca2c94fa34a6de51c004
Griffin <griffincriste@gmail.com>
2020-04-22 10:58:17 -0500

    working

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,56 +1,27 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { User } from '../../models/User';
 
 @Component({
-    selector: 'app-user',
-    templateUrl: './user.component.html',
-    styleUrls: ['./user.component.css'],
-    // template:'<h2>John Doe</h2>',
-    // styles:[`
-    // h2{
-    //     color:blue
-    // }`]
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
 })
 
-export class UserComponent implements OnInit{
-    //properties
-    // firstName: string;
-    // lastName: string;
-    // age: number;
-    // address;
+export class UserComponent implements OnInit {
+  // Properties
+  user: User;
 
-    // foo: any;
-    // hasKids: boolean;
-    // numberArray: number[];
-    // stringArray: string[];
-    // mixedArray: any[];
-    // myTuple: [string, number, boolean];
-    // unusuable: void;
-    // u: undefined;
-    // n: null;
+  // Methods
+  constructor() {
+    
+  } 
 
-    user: User;
-
-
-    //methods
-    constructor() {
-     
+  ngOnInit() {
+    this.user = {
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john@gmail.com'
     }
-
-    ngOnInit(){
-        this.user = {
-            firstName: 'Griffin',
-            lastName: 'Criste',
-            age: 29,
-            address:{
-                street: '50 main street',
-                city: 'Nashville',
-                state: 'TN'
-            }
-        }
-
-    }
-
- 
+  }
 }
-
