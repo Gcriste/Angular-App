f5b4b79c081e1c96860e27769a0530412c61c434
Griffin <griffincriste@gmail.com>
2020-04-22 10:38:19 -0500

    adding click events

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -9,7 +9,7 @@ import { User } from '../../models/User';
 })
 export class UsersComponent implements OnInit {
   users: User[];
-  showExtended: boolean = false;
+  showExtended: boolean = true;
   loaded: boolean = false;
   enableAdd: boolean = true;
 
@@ -28,7 +28,8 @@ export class UsersComponent implements OnInit {
             state: 'MA'
           },
           isActive: true,
-          registered: new Date('01/02/2018 08:30:00')
+          registered: new Date('01/02/2018 08:30:00'),
+          hide: true
         },
         {
           firstName: 'Kevin',
@@ -40,7 +41,8 @@ export class UsersComponent implements OnInit {
             state: 'MA'
           },
           isActive: false,
-          registered: new Date('03/11/2017 06:20:00')
+          registered: new Date('03/11/2017 06:20:00'),
+          hide: true
         },
         {
           firstName: 'Karen',
@@ -52,7 +54,8 @@ export class UsersComponent implements OnInit {
             state: 'FL'
           },
           isActive: true,
-          registered: new Date('11/02/2016 10:30:00')
+          registered: new Date('11/02/2016 10:30:00'),
+          hide: true
         }
       ];
 
@@ -62,9 +65,9 @@ export class UsersComponent implements OnInit {
   addUser(user: User) {
     this.users.push(user);
   }
-  
-  fireEvent(e){
-    // console.log("Button clicked")
-    console.log(e.type)
-  }
+
+
+  // toggleHide(user: User){
+  //   user.hide = !user.hide;
+  // }
 }
