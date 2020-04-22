8fdef8ce463c96b1c53d6829adc2cba11235c001
Griffin <griffincriste@gmail.com>
2020-04-22 09:42:30 -0500

    button event

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -9,7 +9,7 @@ import { User } from '../../models/User';
 })
 export class UsersComponent implements OnInit {
   users: User[];
-  showExtended: boolean = true;
+  showExtended: boolean = false;
   loaded: boolean = false;
   enableAdd: boolean = true;
 
@@ -63,4 +63,8 @@ export class UsersComponent implements OnInit {
     this.users.push(user);
   }
   
+  fireEvent(e){
+    // console.log("Button clicked")
+    console.log(e.type)
+  }
 }
